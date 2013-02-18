0a5892c531e9f75da01f8486df356964afbb052b
Solomon <solomon.kahn@gmail.com>
2013-02-18 13:15:27 -0500

    do some more work

--- public/budget.js
+++ public/budget.js
@@ -1,10 +1,44 @@
-var a = 5;
+var w = 1280 - 80,
+    h = 800 - 180,
+    x = d3.scale.linear().range([0, w]),
+    y = d3.scale.linear().range([0, h]),
+    color = d3.scale.category20c(),
+    root,
+    node;
+
+var treemap = d3.layout.treemap()
+    .round(false)
+    .size([w, h])
+    .sticky(true)
+    .value(function(d) { return d.size; });
+
+var svg = d3.select("#body").append("div")
+    .attr("class", "chart")
+    .style("width", w + "px")
+    .style("height", h + "px")
+  .append("svg:svg")
+    .attr("width", w)
+    .attr("height", h)
+  .append("svg:g")
+    .attr("transform", "translate(.5,.5)");
+
+
 
 line_items = []
 d3.csv('/us_budget_expenses_2013.csv', function(csv){
   $.each(csv, function(row, data){
     line_items.push(data);
   });
+
+  var agencyName = d3.nest()
+    .key(function(d) {return d['Agency Name'];})
+    .key(function(d) {return d['Bureau Name'];})
+    //.sortKeys(d3.ascending)
+    .entries(csv);
+
+  var nodes = treemap.nodes(agencyName)
+
+  console.log(nodes);
 });
 
 var getField = function(item, field){
