5a1419aef334b10ffeeb48e52daef419cfbe91be
Solomon <solomon.kahn@gmail.com>
2013-03-17 12:33:46 -0400

    remove non positive amounts from data, and fix all the treemap errors from negative sized rects

--- public/budget.js
+++ public/budget.js
@@ -33,7 +33,13 @@
     var yearly_budget = line_items.map(
       function(x) { return getYearlyLineItem(x, year)}
     );
-    return yearly_budget;
+
+    var noZeroSize = _.filter(yearly_budget, function(x){ return x.size > 0;});
+    return noZeroSize;
+  };
+
+  var filterZeroSize = function(nestedData){
+
   };
 
   var getYearlyData = function(year){
@@ -43,7 +49,7 @@
       var agencyChildren = getAgencyChildren(val);
       return {
         "name" : key,
-        //"children" : agencyChildren,
+        "children" : agencyChildren,
         "size" : _.reduce(agencyChildren, function(sum, num){
         return sum + num.size;},0)
       };
@@ -67,7 +73,7 @@
    var agency_children = _.map(r, function(val, key){
      return {
        "name" : key,
-       //"children" : val,
+       "children" : val,
        "size" : _.reduce(val, function(sum, num){
          return sum + num.size;}, 0)
      };
@@ -103,10 +109,10 @@
         .attr("transform", "translate(.5,.5)");
 
     var mainData = getYearlyData('2010');
-    console.log(mainData);
+    //console.log(mainData);
     var nodes = treemap.nodes(mainData);
 
-    console.log(nodes);
+    //console.log(nodes);
 
     var cell = svg.selectAll("g")
         .data(nodes)
@@ -116,8 +122,8 @@
         .on("click", function(d) { return zoom(node == d.parent ? root : d.parent); });
 
     cell.append("svg:rect")
-        .attr("width", function(d) { return d.dx - 1; })
-        .attr("height", function(d) { return d.dy - 1; })
+        .attr("width", function(d) { return d.dx ; })
+        .attr("height", function(d) { return d.dy ; })
         .style("fill", function(d) { return color(d.size); });
 
     cell.append("svg:text")
@@ -125,31 +131,48 @@
         .attr("y", function(d) { return d.dy / 2; })
         .attr("dy", ".35em")
         .attr("text-anchor", "middle")
-        .text(function(d) { return d.name; })
+        .text(function(d) {
+          var msg = d.name;
+          if(d.size){
+            msg += " : $";
+            msg += d.size.toFixed(0).replace(/(\d)(?=(\d{3})+\b)/g,'$1,');
+          }
+          return msg;
+        })
         .style("opacity", function(d) { d.w = this.getComputedTextLength(); return d.dx > d.w ? 1 : 0; });
+
+    cell.append("svg:title")
+        .text(function(d) {
+          var msg = d.name;
+          if(d.size){
+            msg += " : $";
+            msg += d.size.toFixed(0).replace(/(\d)(?=(\d{3})+\b)/g,'$1,');
+          }
+          return msg;
+        });
 
     var size = function(d){return d.size;};
 
-    var zoom = function(d) {
-      var kx = w / d.dx, ky = h / d.dy;
-      x.domain([d.x, d.x + d.dx]);
-      y.domain([d.y, d.y + d.dy]);
+    //var zoom = function(d) {
+      //var kx = w / d.dx, ky = h / d.dy;
+      //x.domain([d.x, d.x + d.dx]);
+      //y.domain([d.y, d.y + d.dy]);
 
-      var t = svg.selectAll("g.cell").transition()
-          .duration(d3.event.altKey ? 7500 : 750)
-          .attr("transform", function(d) { return "translate(" + x(d.x) + "," + y(d.y) + ")"; });
+      //var t = svg.selectAll("g.cell").transition()
+          //.duration(d3.event.altKey ? 7500 : 750)
+          //.attr("transform", function(d) { return "translate(" + x(d.x) + "," + y(d.y) + ")"; });
 
-      t.select("rect")
-          .attr("width", function(d) { return kx * d.dx - 1; })
-          .attr("height", function(d) { return ky * d.dy - 1; })
+      //t.select("rect")
+          //.attr("width", function(d) { return kx * d.dx - 1; })
+          //.attr("height", function(d) { return ky * d.dy - 1; })
 
-      t.select("text")
-          .attr("x", function(d) { return kx * d.dx / 2; })
-          .attr("y", function(d) { return ky * d.dy / 2; })
-          .style("opacity", function(d) { return kx * d.dx > d.w ? 1 : 0; });
+      //t.select("text")
+          //.attr("x", function(d) { return kx * d.dx / 2; })
+          //.attr("y", function(d) { return ky * d.dy / 2; })
+          //.style("opacity", function(d) { return kx * d.dx > d.w ? 1 : 0; });
 
-      node = d;
-      d3.event.stopPropagation();
-    }
+      //node = d;
+      //d3.event.stopPropagation();
+    //}
   };
 //});
